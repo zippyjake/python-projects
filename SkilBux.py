2cf087a585c49f48ff9f4799e6b64733f41aac84
zippyjake <zippyjake1@gmail.com>
2014-09-09 19:04:12 -0400

    skilbucks
    started a bank program

---
+++ SkilBux.py
@@ -0,0 +1,24 @@
+rates = {
+    '1 hour of class': 3,
+    '1 daily GitHub green dot': 2,
+    'bads words': -10,
+    '
+    
+}
+
+rates
+
+
+
+
+earnables = {
+    {
+         'name':'1 hour of minecraft camp': 15:,
+         'hourly' 15,
+         'limited' = False
+         
+    '2 hour saturday MC camp': 30:,
+    '5 day summer mc camp (25 hrs)': 375,
+    'Skilstak Basic tee shirt': 30,
+    'Lego Minecraft' 
+}

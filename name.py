fea8a864c45c39931febc4b055de6cf136b22ae4
zippyjake <zippyjake1@gmail.com>
2014-10-07 18:45:58 -0400

    name
    got back to python

---
+++ name.py
@@ -0,0 +1,11 @@
+while True:
+    name = input("What is your name ").strip().lower()
+
+    if 'J' in name:
+        won = input("You won a million dollars!!!!!!!!!!!!!!! ").strip().lower()
+
+        if 'y' in won:
+            print("Jusst kidding...... ")
+        
+        
+        

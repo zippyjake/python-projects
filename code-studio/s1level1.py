72988ba876fdebbca4372abee161a988a647f72b
zippyjake <zippyjake1@gmail.com>
2014-10-11 10:34:10 -0400

    TURTLE!!!!!!!!!!!!!!!!!!!!!!
    did python turtle

---
+++ code-studio/s1level1.py
@@ -0,0 +1,16 @@
+'''
+
+moveForward(100);
+turnRight(90);
+moveForward(100);
+'''
+
+from turtle import Turtle
+
+youranus = Turtle()
+youranus.pensize(7)
+youranus.pencolor('indigo')
+
+youranus.forward(100)
+youranus.right(90)
+youranus.forward(100)
